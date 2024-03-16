f6aa6452da59fe96fe51ec3a4401ac6d0ac16cb2
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-03-16 19:51:41 -0300

    Add test support

--- lua/Weslyn/lazy.lua
+++ lua/Weslyn/lazy.lua
@@ -14,6 +14,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.util.dot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns"},
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "plugins" },
   },
   defaults = {
