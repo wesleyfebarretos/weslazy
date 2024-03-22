7743735efdf490e9eb204634cd9ebab7dd98e923
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-03-21 22:24:20 -0300

    Add Typescript Barrel To Neo Tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -32,6 +32,51 @@ local function get_folder_node(state)
     end
 end
 
+local function typescriptBarrel(dirPath)
+    -- Lista diretórios e arquivos .ts, excluindo o próprio index.ts
+    local commandDirs = 'find "' .. dirPath .. '" -mindepth 1 -maxdepth 1 -type d'
+    local commandFiles = 'find "' .. dirPath .. '" -mindepth 1 -maxdepth 1 -type f -name "*.ts" ! -name "index.ts"'
+    local exports = {}
+    
+    -- Processa diretórios
+    local pDirs = io.popen(commandDirs)
+
+    for subdir in pDirs:lines() do
+        local subdirName = subdir:match("^.+/(.+)$") -- Extrai o nome do subdiretório
+        local indexPath = subdir .. "/index.ts"
+        local f = io.open(indexPath, "r")
+        if f then
+            io.close(f)
+            table.insert(exports, "export * from './" .. subdirName .. "';")
+        end
+    end
+    pDirs:close()
+
+    -- Processa arquivos
+    local pFiles = io.popen(commandFiles)
+    for file in pFiles:lines() do
+        local fileName = file:match("^.+/(.+)$") -- Extrai apenas o nome do arquivo
+        table.insert(exports, "export * from './" .. fileName:gsub("%.ts$", "") .. "';")
+    end
+    pFiles:close()
+
+    if next(exports) ~= nil then
+        -- Escreve os exports no index.ts do diretório atual
+        local outputPath = dirPath .. "/index.ts"
+        local content = table.concat(exports, "\n")
+        local outputFile, err = io.open(outputPath, "w")
+
+        if not outputFile then
+            print("Erro ao abrir arquivo para escrita: ", err)
+            return
+        end
+
+        outputFile:write(content)
+        outputFile:close()
+        -- print("index.ts foi criado com sucesso em: ", dirPath)
+    end
+end
+
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -43,8 +88,8 @@ return {
                     local node = get_folder_node(state)
                     local dir = node:get_id()
 
-                    print(dir)
-                end,
+                    typescriptBarrel(dir)
+                end
             },
         },
     },
