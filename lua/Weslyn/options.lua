3cb2e36f3dd3142f4e731ec67949bd34200a5b25
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-03-23 15:07:40 -0300

    fix: customize colors schemes

--- lua/Weslyn/options.lua
+++ lua/Weslyn/options.lua
@@ -2,9 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Set vim background
+-- Set vim line numbers background
 vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bg = "none" })
-vim.api.nvim_set_hl(0, "Normal", { bg = "none"  })
 
 -- This file is automatically loaded by plugins.core
 vim.g.mapleader = " "
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -3,6 +3,11 @@ return {
         "rebelot/kanagawa.nvim",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
+        opts = function()
+            return {
+                transparent = true
+            }
+        end,
         config = function()
             -- load the colorscheme here
             -- vim.cmd([[colorscheme kanagawa-dragon]])
@@ -18,4 +23,14 @@ return {
             },
         },
     },
+    {
+        "craftzdog/solarized-osaka.nvim",
+        lazy = true,
+        priority = 1000,
+        opts = function ()
+            return {
+                transparent = true,
+            }
+        end
+    }
 }
