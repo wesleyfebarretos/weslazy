ac0caf226998e9f6aa4f445fc8222405c3a8a6f6
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-03-23 00:43:49 -0300

    style: Set Vim Background

--- lua/Weslyn/options.lua
+++ lua/Weslyn/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Set vim background
+vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none"  })
+
 -- This file is automatically loaded by plugins.core
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
