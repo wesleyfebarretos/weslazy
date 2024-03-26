0806890b03eec419dc27b8897774770f90f6ed4c
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-03-26 01:16:27 -0300

    feat: Add vim tmux navigator plugin

--- lua/Weslyn/lazy.lua
+++ lua/Weslyn/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
         { import = "lazyvim.plugins.extras.test.core" },
         { import = "lazyvim.plugins.extras.dap.core" },
+        { import = "lazyvim.plugins.extras.lang.go" },
         { import = "plugins" },
     },
     defaults = {
