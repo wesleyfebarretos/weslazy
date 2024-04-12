f3212ab1fdd80cd7f78ca981afd9fafb110abe2d
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-04-11 22:17:59 -0300

    feat: remove css from auto indent

--- lua/Weslyn/autocmds.lua
+++ lua/Weslyn/autocmds.lua
@@ -1,6 +1,6 @@
 -- Disable auto format for specific languages
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "typescript", "javascript", "json" },
+    pattern = { "typescript", "javascript", "json", "css" },
     callback = function()
         vim.b.autoformat = false
     end,
