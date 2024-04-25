3c5c78ede68246923f399770c48267bb39ed1db3
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-04-25 12:27:24 -0300

    feat: remove diagnostic to env file

--- lua/Weslyn/autocmds.lua
+++ lua/Weslyn/autocmds.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
         vim.b.autoformat = false
     end,
 })
+
+local group = vim.api.nvim_create_augroup("__env", { clear = true })
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = ".env",
+    group = group,
+    callback = function(args)
+        vim.diagnostic.disable(args.buf)
+    end,
+})
