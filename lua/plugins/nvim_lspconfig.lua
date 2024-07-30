dcb16be7ecd5069a8ab8e8f21287c0a4614efa73
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-07-30 19:07:44 -0300

    fix: mason and lsp config auto install

---
+++ lua/plugins/nvim_lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            mason = false,
+            servers = {
+                "*" == {
+                    mason = false,
+                },
+            },
+        },
+    },
+}
