81e7fef9af4563e91ca75190a646092de590d491
Wesley Ferreira <wesleyfebarretos@gmail.com>
2024-09-07 17:05:14 -0300

    style: change theme

--- lua/Weslyn/lazy.lua
+++ lua/Weslyn/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
             "LazyVim/LazyVim",
             import = "lazyvim.plugins",
             opts = {
-                colorscheme = "catppuccin-macchiato",
+                colorscheme = "catppuccin-mocha",
             },
         },
         { import = "lazyvim.plugins.extras.lang.typescript" },
