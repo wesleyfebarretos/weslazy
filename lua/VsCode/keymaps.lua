2c68f1c8409755165d5723f490d460fd97289a3c
Wesley Ferreira <wesleyfebarretos@gmail.com>
2025-02-18 14:38:56 -0300

    fix

--- lua/VsCode/keymaps.lua
+++ lua/VsCode/keymaps.lua
@@ -1,5 +1,13 @@
+local nnoremap = require("Weslyn.keymaps_utils").nnoremap
+local vnoremap = require("Weslyn.keymaps_utils").vnoremap
+local inoremap = require("Weslyn.keymaps_utils").inoremap
+local vscode = require("vscode-neovim")
+
 local M = {}
 
 vim.keymap.set({ "i", "n", "v", "x", "s", "o", "c", "t" }, "<C-[>", "<Esc>", { noremap = true, silent = true })
+
+nnoremap("p", "pgvy", { desc = "paste without override current registrie" })
+nnoremap("P", "Pgvy", { desc = "paste without override current registrie" })
 
 return M
